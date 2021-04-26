c412c8417fc03b972d5f4f1ae05e0ef5b677624f
miau <notmiau@protonmail.com>
2021-04-26 02:28:51 +0100

    addition: 8ball command

---
+++ src/commands/Fun/8Ball.ts
@@ -0,0 +1,33 @@
+import { Command } from 'discord-akairo'
+import { Message } from 'discord.js'
+
+import { eightBallReplies } from '../../utilities/Constants'
+
+export default class EightBall extends Command {
+    public constructor() {
+        super('8ball', {
+            aliases: ['8ball', '8b', 'eightball'],
+            category: 'Fun',
+            description: [
+                {
+                    content: 'Ask the 8ball a question and see what answer you get back!',
+                    usage: '8ball [question]',
+                    examples: ['8ball Do I have friends?']
+                }
+            ],
+            clientPermissions: ['SEND_MESSAGES'],
+            ratelimit: 5,
+            args: [
+                {
+                    id: 'question',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+
+    public exec(message: Message, {question}: {question: string}): Promise<Message> {
+        return message.util!.reply(question ? eightBallReplies[Math.floor(Math.random() * eightBallReplies.length)] : 'What are you asking the 8ball?')
+
+    }
+}
