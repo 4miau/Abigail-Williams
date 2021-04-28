97948d6ac63e66ef000d9fa769dd3d2bc4afb13a
miau <notmiau@protonmail.com>
2021-04-28 21:47:45 +0100

    updated packages and constants

--- src/utilities/Constants.ts
+++
@@ -1,5 +0,0 @@
-export const eightBallReplies: string[] = [
-    'Yes', 'No', 'Maybe', 'Probably', 'Probably not', 'It is certain, yes', 'I can tell you certainly, no', 'Without a doubt, yes', 'Definitely', 'Definitely not',
-    'Ask again later', 'Try again later', 'It is likely', 'It is unlikely', 'Fortunately, yes', 'Unfortunately, no', 'I have decided it is so', 'I have decided it is not',
-    'Without a doubt', 'I am uncertain', 'I can not be sure about that'
-]
