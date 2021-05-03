aa0a5261294461990977d9ecf1c74f60dc661783
miau <https://notmiau@protonmail.com>
2021-05-03 02:38:48 +0100

    update: uses embed, fixed error handling

---
+++ src/commands/Fun/Gayrate.ts
@@ -0,0 +1,49 @@
+import { Command } from 'discord-akairo'
+import { GuildMember, GuildMemberResolvable, Message, MessageEmbed } from 'discord.js'
+
+export default class Gayrate extends Command {
+    public constructor() {
+        super('gayrate', {
+            aliases: ['gayrate'],
+            category: 'Fun',
+            description: [
+                {
+                    content: 'Returns a rating of how gay a user is (between 0-100%)',
+                    usage: ['gayrate [@user]'],
+                    examples: ['gayrate @user']
+                }
+            ],
+            clientPermissions: ['EMBED_LINKS'],
+            ratelimit: 3,
+            args: [
+                {
+                    id: 'member',
+                    type: 'member',
+                    default: (msg: Message) => msg.member
+                }
+            ]
+        })
+    }
+
+    public exec(message: Message, {member}: {member: GuildMemberResolvable}): Promise<Message> {
+        const userResolved: GuildMember = message.guild.members.resolve(member)
+
+        const gayRate: number = Math.floor(Math.random() * 100)
+
+        if (userResolved && userResolved.id !== message.author.id) {
+            return message.util!.send(new MessageEmbed()
+                .setTitle('Gayrate Result')
+                .setColor('RANDOM')
+                .addField('gayRate', `${userResolved.displayName} is ${gayRate}% gay`, true)
+                .setTimestamp(Date.now())
+            )
+        } else {
+            return message.util!.send(new MessageEmbed()
+                .setTitle('Gayrate Result')
+                .setColor('RANDOM')
+                .addField('gayRate', `you are ${gayRate}% gay`, true)
+                .setTimestamp(Date.now())
+            )
+        }
+    }
+}
---
+++ src/commands/Fun/Ship.ts
@@ -0,0 +1,44 @@
+import { Command } from 'discord-akairo'
+import { GuildMemberResolvable, GuildMember, Message, MessageEmbed } from 'discord.js'
+import { getRandomIntRange } from '../../utils/Functions'
+
+export default class Ship extends Command {
+    public constructor() {
+        super('ship', {
+            aliases: ['ship'],
+            category: 'Fun',
+            description: [
+                {
+                    content: 'Ships between 2 users',
+                    usage: 'ship [@user1] [@user2]',
+                    examples: ['ship @user1 @user2']
+                }
+            ],
+            ratelimit: 3,
+            args: [
+                {
+                    id: 'memberOne',
+                    type: 'member'
+                },
+                {
+                    id: 'memberTwo',
+                    type: 'member'
+                }
+            ]
+        })
+    }
+
+    public exec(message: Message, {memberOne, memberTwo}: {memberOne: GuildMemberResolvable, memberTwo: GuildMemberResolvable}): Promise<Message> {
+        const userOne: GuildMember = message.guild.members.resolve(memberOne)
+        const userTwo: GuildMember = message.guild.members.resolve(memberTwo)
+
+        if (userOne && userTwo) {
+            return message.util!.send(new MessageEmbed()
+                .setTitle('The ship has set sail')
+                .setDescription(`:ship: ${userOne} and ${userTwo} have a ship level of ${getRandomIntRange(0, 99)}%`)
+            )
+        } else {
+            message.util!.send('Please provide 2 members to ship.')
+        }
+    }
+}
