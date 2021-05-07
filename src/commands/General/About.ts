906b7dedcb2f3d12ade42b5e44dc8a928564d3d1
miau <https://notmiau@protonmail.com>
2021-05-07 02:10:51 +0100

    add: About WIP

---
+++ src/commands/General/About.ts
@@ -0,0 +1,34 @@
+import { Command } from 'discord-akairo'
+import { Message, MessageEmbed } from 'discord.js'
+import { Colours } from '../../utils/Colours'
+import { secondsConvert } from '../../utils/Constants'
+
+export default class About extends Command {
+    public constructor() {
+        super('about', {
+            aliases: ['about', 'botinfo'],
+            category: 'General',
+            description: [
+                {
+                    content: 'Displays information about the bot',
+                    usage: 'about',
+                    examples: ['about']
+                }
+            ],
+            ratelimit: 3
+        })
+    }
+
+    public exec(message: Message): Promise<Message> {
+        return message.util!.send(new MessageEmbed()
+            .setAuthor('abby-bot - About', message.guild.me.user.avatarURL({ 'dynamic': true }), message.guild.me.user.avatarURL({dynamic: true}))
+            .setColor(Colours.NovaGreen)
+            .setDescription(`miau's discord bot, brrrrrrrrrr`)
+            .addField('Uptime', `${this.client.uptime / secondsConvert}s`, true)
+            .addField('Latency', `${this.client.ws.ping}ms`, true)
+            .addField('Version', '0.3.0', true) //IMPLEMENT AUTO UPDATE OF VERSION
+            .addField('Author', `${this.client.users.resolve(this.client.ownerID.toString()).username}`, false)
+            .addField('Support the bot!', 'SUPPORT LINK HERE OR SMTH', false)
+        )
+    }
+}
