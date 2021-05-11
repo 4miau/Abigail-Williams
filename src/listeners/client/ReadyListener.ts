c07ca966cc789efe25f8f6f6a9a2eead573b8cb8
miau <https://notmiau@protonmail.com>
2021-05-11 01:47:48 +0100

    add: winston logs & twitch live detection

--- src/listeners/client/ReadyListener.ts
+++ src/listeners/client/ReadyListener.ts
@@ -1,6 +1,8 @@
 import { Listener } from 'discord-akairo'
 import winston from 'winston'
 
+import { _GetUser } from '../../utils/Functions';
+
 export default class ReadyListener extends Listener {
     public constructor() {
         super('ready', {
@@ -19,6 +21,31 @@ export default class ReadyListener extends Listener {
         })
 
         logger.log('info', `${this.client.user.tag} has successfully connected.`)
+
+        //Twitch Configuration Settings if non-existent
+        ;(async () => {
+            for (const guild of this.client.guilds.cache) {
+                const twitchMessage = this.client.settings.get(guild[1], 'twitch.twitch-message', '')
+                if (!twitchMessage) this.client.settings.set(guild[1], 'twitch.twitch-message', `{streamer} has gone live! {link}`)
+                else continue
+            }
+        })() 
+        //
+
+        setInterval(async () => {
+            this.client.guilds.cache.forEach(guild => {
+                const streamers: string[] = this.client.settings.get(guild, 'twitch.twitch-users', [])
+                if (streamers) {
+                    streamers.forEach(async streamer => {
+                        return await _GetUser(streamer)
+                            .then(user => {
+                                logger.log('info', user.broadcaster_login + ': ' + user.is_live)
+                                return user.is_live
+                            })
+                    })
+                }
+            });
+        }, 6e5)
     }
 }
 
@@ -47,5 +74,4 @@ import { BotStatus } from '../../models/BotStatus'
                 ])
 
         }
-        
 */
