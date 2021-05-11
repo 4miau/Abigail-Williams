378ad56c6ca1277c6bd9ba4ee74ce6f7e7515eb3
miau <https://notmiau@protonmail.com>
2021-05-11 01:46:06 +0100

    update: roles count properly, tags & flags are shown

--- src/commands/General/About.ts
+++ src/commands/General/About.ts
@@ -1,5 +1,7 @@
 import { Command } from 'discord-akairo'
 import { Message, MessageEmbed } from 'discord.js'
+import { stripIndents } from 'common-tags'
+
 import { Colours } from '../../utils/Colours'
 import { secondsConvert } from '../../utils/Constants'
 
@@ -22,14 +24,14 @@ export default class About extends Command {
             .setAuthor('abby-bot - About', message.guild.me.user.avatarURL({ 'dynamic': true }), message.guild.me.user.avatarURL({dynamic: true}))
             .setColor(Colours.NovaGreen)
             .setDescription(`**miau's discord bot.**`)
-            .addField('Uptime', `${this.client.uptime / secondsConvert}s`, true)
+            .addField('Uptime', `${(this.client.uptime / secondsConvert).toFixed(2)}s`, true)
             .addField('Latency', `${this.client.ws.ping}ms`, true)
-            .addField('Version', '0.3.0', true) //IMPLEMENT AUTO UPDATE OF VERSION
+            .addField('Version', '0.0.1 (will add versions)', true) //IMPLEMENT AUTO UPDATE OF VERSION
             .addField('Author', `${this.client.users.resolve(this.client.ownerID.toString()).username}`, false)
-            .addField('Support the bot!', `
-                    I appreciate donations & support to help maintain the bot. Feel free to donate if you have even the slightest intention.\n
+            .addField('Support the bot!', (stripIndents`
+                    I appreciate donations & support to help maintain the bot. Feel free to donate if you have even the slightest intention.
                     https://ko-fi.com/notmiauu
-                `, false)
+                `), false)
         )
     }
 }
--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -23,13 +23,6 @@ export default class Help extends Command {
                     default: null
                 },
                 {
-                    id: 'page',
-                    type: (_: Message, str: string): null | number => {
-                        if (parseInt(str)) return Number(str)
-                        return null
-                    }
-                },
-                {
                     id: 'dms',
                     type: 'string',
                     match: 'option',
@@ -44,7 +37,7 @@ export default class Help extends Command {
             return message.util!.send(new MessageEmbed()
                 .setAuthor(`Help | ${command}`, this.client.user.displayAvatarURL())
                 .setColor(Colours.Mint)
-                .setDescription(stripIndents`
+                .setDescription(`
                     **Description:**
                     ${command.description.content || 'No content provided'}
 
@@ -53,8 +46,18 @@ export default class Help extends Command {
 
                     **Examples:**
                     ${command.description.examples ? command.description.examples.map((e: any) => `\`${e}\``).join('\n') : 'No examples provided.'}
+                    \
+                    ${command.description.flags ?
+                    `**Flags:**       
+                    ${command.description.flags.join(', ')}
+                    `: ''}
+                    \
+                    ${command.description.tags ? `**Tags:**
+                    ${command.description.tags.join('\n')}
+                    `:''}
+                    
                 `)
-                .setFooter('Syntax: [] = required, <> = optional')
+                .setFooter('Syntax: [] = required, <> = optional, {} = optional tag')
             )
         }
 
@@ -78,71 +81,3 @@ export default class Help extends Command {
         return message.channel.send(embed)
     }
 }
-
-
-/*
-import { Client, Message, MessageEmbed } from "discord.js";
-import CommandStore from "../../stores/CommandStore";
-export async function run (msg: Message, args: string[]) {
-    const cmds = Array.from(CommandStore.values())
-
-    const obj = {}
-
-    for (const cmd of cmds.sort((a,b)=>a.config.name.localeCompare(b.config.name))) {
-        if (!obj[cmd.config.category]) obj[cmd.config.category] = []
-        obj[cmd.config.category].push(cmd.config.name)
-    }
-
-    msg.channel.send(
-        new MessageEmbed().setColor("#C3B1E1").setDescription(Object.keys(obj).sort((a,b)=>a.localeCompare(b)).map(key=>`**${key}**\n\`${obj[key].join('`, `')}\``).join('\n\n'))
-    )
-}
-
-export const config = {
-    aliases: ['h']
-}
-*/
-
-/*
-    private validatePage(page: number, uid: string): number | null {
-        if (page === 3 && uid === this.client.ownerID) return page
-        if (pages.includes(page) && uid) return page
-        return 1
-    }
-
-        //Validation before retrieving the help messages
-        if (!isNaN(parseInt(command)) && !page) {
-            page = parseInt(command)
-            command = null
-
-            if (page) page = this.validatePage(page, message.author.id)
-        } else if (command && page) {
-            page = null
-        } else page = 1 
-
-        //Help embeds
-        if (!command && page === 1) {
-            return message.util!.send(new MessageEmbed()
-                .setAuthor('Standard Commands', 'https://i.imgur.com/1T4RT2w.png', commandsGithub)
-                .setDescription(`Type ${this.client.settings.get(message.guild, 'config.prefix', 'a.')}help [command] for more help on a command`)
-                .addField('General', '`help`', true)
-                .addField('Fun', '`rr`,`8ball`,`gayrate`', true)
-                .addField('Utility', '`ping`,`avatar`,`uptime`', true)
-                .addField('\u200B', '\u200B')
-                .setFooter(`Type ${this.client.settings.get(message.guild, 'config.prefix', 'a.')}help 2 for mod & config commands.`)
-            )
-        } else if (!command && page === 2) {
-            return message.util!.send(new MessageEmbed()
-                .setAuthor('Moderator Commands', 'https://i.imgur.com/1T4RT2w.png', commandsGithub)
-                .setDescription(`Type ${this.client.settings.get(message.guild, 'config.prefix', 'a.')}help [command] for more help on a command`)
-                .addField('Moderation', '`ban`,`unban`,`kick`,`warn`,\n`fixname`,`slowmode`,`prune (WIP)`', true)
-                .addField('Configuration', '`setmuterole`', true)
-            )
-        } else if (!command && page === 3 && message.author.id === this.client.ownerID) {
-            return message.util!.send(new MessageEmbed()
-                .setAuthor('Owner Commands', 'https://i.imgur.com/1T4RT2w.png', commandsGithub)
-                .setDescription(`Hiya there Miau! You're pog!`)
-                .addField('Owner', '`activity`,`build`,`eval`,`restart`,\n`shutdown`,`status`', true)
-            )
-        }
-*/
