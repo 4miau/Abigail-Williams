129ab8daef6380d6ce05bcf6adf449049ebbdbc8
miau <https://notmiau@protonmail.com>
2021-05-17 07:40:53 +0100

    upd: bunny api back up, added -gif tag

--- src/commands/Images/Bunny (best cmd).ts
+++ src/commands/Images/Bunny (best cmd).ts
@@ -8,25 +8,43 @@ export default class Bunny extends Command {
             aliases: ['bunny', 'bunnies'],
             category: 'Images',
             description: {
-                content: 'Posts an image of a bunny, best command to be honest.',
+                content: 'Posts an image of a bunny, best command to be honest. (add -gif for a gif instead of a png)',
                 usage: 'bunny',
-                examples: ['bunny'],
+                examples: ['bunny', 'bunny -gif'],
+                flags: ['-gif']
             },
             ratelimit: 3,
+            args: [
+                {
+                    id: 'gif',
+                    match: 'flag'
+                }
+            ]
         })
     }
 
-    public async exec(message: Message): Promise<Message> {
+    public async exec(message: Message, {gif}: {gif: boolean}): Promise<Message> {
         const bunny = await axios.get('https://api.bunnies.io/v2/loop/random/?media=gif,png', {
             'method': 'GET'
         })
-        .then(res => res.data)
+        .then(res => res.data.media)
+
+        if (gif) {
+            return message.util!.send(new MessageEmbed()
+                .setDescription('Here\'s a bunny! The best animal, totally not bias!')
+                .setColor('RANDOM')
+                .setImage(bunny.gif)
+                .setFooter(`#${bunny.id}`)
+            )
+        } else {
+            return message.util!.send(new MessageEmbed()
+                .setDescription('Here\'s a bunny! The best animal, totally not bias!')
+                .setColor('RANDOM')
+                .setImage(bunny.poster)
+                .setFooter(`#${bunny.id}`)
+            )   
+        }
         
-        return message.util!.send(new MessageEmbed()
-            .setDescription('Here\'s a bunny! The best animal, totally not bias!')
-            .setColor('RANDOM')
-            .setImage(bunny.media.gif)
-            .setFooter(`#${bunny.id}`)
-        )
+
     }
 }
