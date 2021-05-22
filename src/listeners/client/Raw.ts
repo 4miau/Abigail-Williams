37b43e5e32df968ec0e32e8c32072fb1dc75e4ce
miau <https://notmiau@protonmail.com>
2021-05-23 00:33:26 +0100

    chore: cleaned super call

--- src/listeners/client/Raw.ts
+++ src/listeners/client/Raw.ts
@@ -4,9 +4,9 @@ import { VoicePacket } from "erela.js";
 export default class Raw extends Listener {
     public constructor() {
         super('raw', {
-            'emitter': 'client',
-            'type': 'client',
-            'event': 'raw',      
+            emitter: 'client',
+            type: 'client',
+            event: 'raw',      
         })
     }
 
