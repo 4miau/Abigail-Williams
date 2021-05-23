c1012677c5b987b6265a704083f74b69f5ed3838
miau <https://notmiau@protonmail.com>
2021-05-23 11:17:23 +0100

    chore: Removed guide comment, cleaned up code

--- src/structures/MemberDataManager.ts
+++ src/structures/MemberDataManager.ts
@@ -39,27 +39,3 @@ export default class MemberDataManager {
         memberDataRepo.save(oldWarn)
     }
 }
-
-/*
-        this.client.logger.log('INFO', `[MUTE] Unmuted ${mute.targetTag} in ${this.client.guilds.resolve(mute.guildID).name}`)
-
-        const caseRepo = this.client.db.getRepository(Case)
-
-        const guild = await this.client.guilds.fetch(mute.guildID)
-        const muteRole = this.client.settings.get(guild, 'muteRole', '')
-        const member = guild.members.resolve(mute.targetID) ? guild.members.resolve(mute.targetID) : void 0
-
-        if (!muteRole) return 'You need to set a mute role before being able to mute a user.'
-        if (!member) return 'Unable to resolve user, most likely no longer in the guild.'
-
-        try { await member.roles.remove(muteRole) }
-        catch { return 'This user is not muted or I am unable to remove the role from this user.'}
-
-        mute.actionComplete = true
-        await caseRepo.save(mute)
-
-        const schedule = this.queuedSchedules.get(mute.id)
-
-        if (schedule) clearTimeout(schedule)
-        return this.queuedSchedules.delete(mute.id
-*/
