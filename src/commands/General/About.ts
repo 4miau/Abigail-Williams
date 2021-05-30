9a80dc8b62f9467b02692bb9fba2eda2341b7b63
miau <https://notmiau@protonmail.com>
2021-05-30 22:59:22 +0100

    fix: uptime tracking properly, new formatting

--- src/commands/General/About.ts
+++ src/commands/General/About.ts
@@ -1,9 +1,11 @@
 import { Command } from 'discord-akairo'
 import { Message, MessageEmbed } from 'discord.js'
 import { stripIndents } from 'common-tags'
+import moment from 'moment'
 
 import { Colours } from '../../util/Colours'
-import { secondsConvert } from '../../util/Constants'
+import { botVersion } from '../../Config'
+
 
 export default class About extends Command {
     public constructor() {
@@ -21,17 +23,19 @@ export default class About extends Command {
 
     public exec(message: Message): Promise<Message> {
         return message.util!.send(new MessageEmbed()
-            .setAuthor('abby-bot - About', message.guild.me.user.avatarURL({ 'dynamic': true }), message.guild.me.user.avatarURL({dynamic: true}))
+            .setAuthor('abby-bot - About', this.client.user.avatarURL({ 'dynamic': true }), this.client.user.avatarURL({dynamic: true, format: 'png' }))
             .setColor(Colours.NovaGreen)
-            .setDescription(`**miau's discord bot.**`)
-            .addField('Uptime', `${(this.client.uptime / secondsConvert).toFixed(2)}s`, true)
+            .setDescription(`**Hi! I'm Abigail Williams, a cute bot created by miau!**`)
+            .setThumbnail(this.client.user.avatarURL({dynamic: true, format: 'png' }))
+            .addField('Uptime', `${moment((this.client.uptime)).format('HH:mm:ss')}s`, true)
             .addField('Latency', `${this.client.ws.ping}ms`, true)
-            .addField('Version', '0.0.1 (will add versions)', true) //IMPLEMENT AUTO UPDATE OF VERSION
+            .addField('Version', `v${botVersion}`, true) //IMPLEMENT AUTO UPDATE OF VERSION
             .addField('Author', `${this.client.users.resolve(this.client.ownerID.toString()).username}`, false)
             .addField('Support the bot!', (stripIndents`
                     I appreciate donations & support to help maintain the bot. Feel free to donate if you have even the slightest intention.
                     https://ko-fi.com/notmiauu
                 `), false)
+            .setFooter('Powered by the cutie miau.')
         )
     }
 }
