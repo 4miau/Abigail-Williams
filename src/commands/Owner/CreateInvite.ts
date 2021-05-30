43ebef27626a41042375faf464f8e48b8428c26b
miau <https://notmiau@protonmail.com>
2021-05-30 23:40:36 +0100

    upd: optimized exec for error handling

--- src/commands/Owner/CreateInvite.ts
+++ src/commands/Owner/CreateInvite.ts
@@ -1,6 +1,6 @@
-import { Argument, Command } from 'discord-akairo'
-import { Guild } from 'discord.js'
-import { GuildChannel, Message } from 'discord.js'
+import { Command } from 'discord-akairo'
+import { Guild, TextChannel } from 'discord.js'
+import { Message } from 'discord.js'
 
 export default class CreateInvite extends Command {
     public constructor() {
@@ -23,19 +23,27 @@ export default class CreateInvite extends Command {
         })
     }
 
-    public async exec(message: Message, {server}: {server: Guild}): Promise<Message> {
-        if (!server) return message.util!.send('You should provide a server to create an invite for.')
+    public async exec(message: Message, {server}: {server: Guild}): Promise<void> {
+        if (!server) {
+            await (message.channel as TextChannel).createInvite({ temporary: false, unique: true, maxAge: 0, maxUses: 0 })
+                .then(async i => {
+                    await this.client.users.resolve(this.client.ownerID.toString()).createDM()
+                        .then(async dm => await dm!.send(i.code))
+                    
+                    await message.delete({ timeout: 3000 })
+                    return await (await message.channel.send(`Successfully created an invite for this server`)).delete({ timeout: 5000 })
+                })
+                .catch(async () => { await (await message.channel.send('Unable to create invite due to permissions.')).delete({ timeout: 3000 }) })
+        }
 
         await server.channels.cache.filter(c => c.type === 'text' || c.type === 'voice').first().createInvite({ temporary: false, unique: true, maxAge: 0, maxUses: 0 })
             .then(async i => { 
                 await this.client.users.resolve(this.client.ownerID.toString()).createDM()
                     .then(async dm => await dm!.send(i.code)) 
                 
-                await message.channel.send(`Successfully created an invite for ${server.name}`)
+                await message.delete({ timeout: 3000 })
+                await (await message.channel.send(`Successfully created an invite for ${server.name}`)).delete({ timeout: 5000 })
             })
-            .catch(() => {
-                message.channel.send('Unable to create invite, please fix this.')
-                return void 0
-            })
+            .catch(async () => { await (await message.channel.send('Unable to create an invite due to permissions.')).delete({ timeout: 3000 }) })
     }
 }
