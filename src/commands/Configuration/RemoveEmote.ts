81f1373a2a030bab217bb9e8f3b299b1695852c1
miau <notmiau@pm.me>
2021-11-25 19:06:03 +0000

    UPD: Change General -> Configuration

--- src/commands/Configuration/RemoveEmote.ts
+++ src/commands/Configuration/RemoveEmote.ts
@@ -5,7 +5,7 @@ export default class RemoveEmote extends Command {
     public constructor() {
         super('removeemote', {
             aliases: ['removeemote', 'removeemoji'],
-            category: 'General',
+            category: 'Configuration',
             description: {
                 content: 'Removes an existing emote from the server.',
                 usage: 'removeemote [emoteName]',
@@ -25,21 +25,21 @@ export default class RemoveEmote extends Command {
 
     //@ts-ignore
     userPermissions(message: Message) {
-        const modRole: string = this.client.settings.get(message.guild, 'modRole', '')
-        const hasStaffRole = message.member.hasPermission('MANAGE_EMOJIS', { checkAdmin: true, checkOwner: true}) || message.member.roles.cache.has(modRole)
+        const modRole = this.client.settings.get(message.guild, 'modRole', '')
+        const hasStaffRole = message.member.permissions.has('MANAGE_EMOJIS_AND_STICKERS', true) || message.member.roles.cache.has(modRole)
 
         if (!hasStaffRole) return 'Moderator'
         return null
     }
 
     public async exec(message: Message, {emoji}: {emoji: GuildEmoji}): Promise<Message> {
-        if (!emoji) return message.util!.send('I can\'t really remove an emote if you don\'t provide the name/id.')
+        if (!emoji) return message.channel.send('I can\'t really remove an emote if you don\'t provide the name/id.')
 
         try {
             await message.guild.emojis.cache.find(e => e === emoji).delete()
-            return message.util!.send(`I have removed the emote \`${emoji.name}\` from the server.`)
+            return message.channel.send(`I have removed the emote \`${emoji.name}\` from the server.`)
         } catch {
-            return message.util!.send('Error removing the provided emote, please try again.')
+            return message.channel.send('Error removing the provided emote, please try again.')
         }
     }
 }
