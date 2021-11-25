1ba9e3cc9c3a63d4a39f9916cd7db207fd145b86
miau <notmiau@pm.me>
2021-11-25 20:39:42 +0000

    UPD: Current avatar, will update soon

--- src/commands/Utility/Avatar.ts
+++ src/commands/Utility/Avatar.ts
@@ -1,5 +1,5 @@
 import { Argument, Command } from 'discord-akairo'
-import { Message, MessageEmbed, User, ImageSize } from 'discord.js'
+import { Message, MessageEmbed, User, GuildMember, AllowedImageSize } from 'discord.js'
 
 export default class Avatar extends Command {
     public constructor() {
@@ -7,11 +7,10 @@ export default class Avatar extends Command {
             aliases: ['avatar', 'pfp', 'icon', 'ava'],
             category: 'Utility',
             description: {
-                    content: 'Gets the avatar of a member',
-                    usage: 'avatar [@user]',
-                    examples: ['avatar @user', 'avatar 1337']
+                    content: 'Gets the avatar of a member (size must be a number and the flag).',
+                    usage: 'avatar [@user] <-size=size>',
+                    examples: ['avatar @user', 'avatar 1337', 'avatar miau -size=']
             },
-            clientPermissions: ['EMBED_LINKS'],
             ratelimit: 3,
             args: [
                 {
@@ -22,7 +21,7 @@ export default class Avatar extends Command {
                 },
                 {
                     id: 'size',
-                    type: (_: Message, str: string): null | Number => {
+                    type: (_: Message, str: string): null | number => {
                         if (str && !isNaN(Number(str)) && [16, 32, 64, 128, 256, 512, 1024, 2048].includes(Number(str))) return Number(str)
                         return null
                     },
@@ -36,19 +35,18 @@ export default class Avatar extends Command {
 
     public exec(message: Message, {member, size}): Promise<Message> {
         try {
-            const userResolved: User = this.client.util!.resolveUser(member, this.client.users.cache, false)
-    
-            return message.util!.send(new MessageEmbed()
-                .setAuthor(`Avatar | ${userResolved.tag}`)
-                .setTitle(`Avatar | ${userResolved.tag}`)
-                .setColor('RANDOM')
-                .setDescription(`${userResolved.tag}'s avatar`)
-                .setImage(userResolved.displayAvatarURL({ format: 'png', size: size as ImageSize, dynamic: true }))
-                .setFooter(`${userResolved.tag}`)
-            )
-        } catch {
-            return message.util!.reply('There was an error retrieving this user, please try again!')
-        }
+            const user: User = this.client.util.resolveUser(member, this.client.users.cache, false)
 
+            const e = new MessageEmbed()
+                .setAuthor(`Avatar | ${user.tag}`)
+                .setColor('RANDOM')
+                .setDescription(`${user.tag}'s avatar`)
+                .setImage(user.displayAvatarURL({ format: 'png', size: size as AllowedImageSize, dynamic: true }))
+                .setFooter(`${user.tag}`)
+                
+            return message.util.send({ embeds: [e] })
+        } catch {
+            return message.util.reply('There was an error retrieving this user, please try again!')
+        }
     }
 }
