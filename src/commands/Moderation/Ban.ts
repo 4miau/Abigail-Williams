66a4ca1ca73eab649d7247dd622080ac1d45db68
miau <notmiau@pm.me>
2021-11-25 19:29:33 +0000

    UPD: Now uses MongoDB

--- src/commands/Moderation/Ban.ts
+++ src/commands/Moderation/Ban.ts
@@ -1,10 +1,8 @@
 import { Command } from 'discord-akairo'
 import { Message, GuildMember } from 'discord.js'
 
-import { Repository } from 'typeorm'
-
-import { Case as Case } from '../../models/Case'
-import ModUtil from '../../util/ModUtil'
+import Case from '../../models/Case'
+import ModUtil from '../../util/structures/ModUtil'
 
 export default class Ban extends Command {
     public constructor() {
@@ -49,18 +47,17 @@ export default class Ban extends Command {
 
     //@ts-ignore
     userPermissions(message: Message) {
-        const modRole: string = this.client.settings.get(message.guild, 'modRole', '')
-        const hasStaffRole = message.member.hasPermission('BAN_MEMBERS', { checkAdmin: true, checkOwner: true}) || message.member.roles.cache.has(modRole)
+        const modRole = this.client.settings.get(message.guild, 'modRole', '')
+        const hasStaffRole = message.member.permissions.has('BAN_MEMBERS', true) || message.member.roles.cache.has(modRole)
 
         if (!hasStaffRole) return 'Moderator'
         return null
     }
 
     public async exec(message: Message, {member, reason, skip}: {member: GuildMember, days: number, reason: string, skip: boolean}): Promise<any> {        
-        const caseRepo: Repository<Case> = this.client.db.getRepository(Case)
         const totalCases: number = this.client.settings.get(message.guild, 'totalCases', 0) + 1
 
-        if (!member!.bannable) return message.util!.send('I do not have the permissions to ban this user.')
+        if (!member.bannable) return message.util.send('I do not have the permissions to ban this user.')
 
         if (skip) {
             await member.ban({days: 7, reason: reason})
@@ -68,43 +65,37 @@ export default class Ban extends Command {
         } else {
             message.channel.send('Are you sure that you would like me to ban that user?')
 
-            const reply = (await message.channel.awaitMessages((msg: Message) => msg.author.id === message.author.id, { 
-                'max': 1, 
-                'time': 30000 
-            })).first().content
+            const filter = (msg: Message) => msg.author.id === message.author.id
+            const reply = (await message.channel.awaitMessages({ filter: filter, max: 1, time: 30000 })).first().content
 
             if (reply && (/^y(?:e(?:a|s)?)?$/i).test(reply)) {
                 await member.ban({days: 7, reason: reason})
                 this.client.settings.set(message.guild, 'totalCases', totalCases)
             } else {
-                return message.util!.send('Moderator has not confirmed the ban, the command has now been cancelled.')
+                return message.util.send('Moderator has not confirmed the ban, the command has now been cancelled.')
             } 
         }
 
-        await message.channel.send('Banning user...')
+        const loading = await message.channel.send('Banning user...')
 
-        const newCase = new Case()
-        
-        newCase.guildID = message.guild.id
-        newCase.messageID = message.id
-        newCase.caseID = totalCases
+        const newCase = await new Case({
+            id: await Case.countDocuments() + 1,
+            guildID: message.guild.id,
+            messageID: message.id,
+            caseID: totalCases,
 
-        newCase.action = ModUtil.CONSTANTS.ACTIONS.BAN
-        newCase.reason = reason
-        
-        newCase.targetID = member.user.id
-        newCase.targetTag = member.user.tag
-        newCase.modID = message.author.id
-        newCase.modTag = message.author.tag
+            action: ModUtil.CONSTANTS.ACTIONS.BAN,
+            reason: reason,
 
-        await caseRepo.save(newCase)
+            targetID: member.user.id,
+            targetTag: member.user.tag,
+            modID: message.author.id,
+            modTag: message.author.tag
+        }).save()
         
         this.client.logger.log('CAUTION', `New case has been saved. ${newCase.targetTag} (${newCase.targetID})`)
 
-        return message.util!.send(`${member.user.id} has successfully been banned from the server.`)
-        
+        loading.delete()
+        return message.util.send(`${member.user.id} has successfully been banned from the server.`)
     }
 }
-
-
-//TODO: Simple optimization on ban
