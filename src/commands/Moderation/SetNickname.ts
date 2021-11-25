3e925ca3d1eaa5323704efd794a8b2989a987fdf
miau <notmiau@pm.me>
2021-11-25 19:54:50 +0000

    UPD: User permissions method

--- src/commands/Moderation/SetNickname.ts
+++ src/commands/Moderation/SetNickname.ts
@@ -1,6 +1,5 @@
 import { Command } from 'discord-akairo'
-import { GuildMember } from 'discord.js'
-import { Message } from 'discord.js'
+import { GuildMember, Message } from 'discord.js'
 
 export default class SetNickname extends Command {
     public constructor() {
@@ -8,12 +7,11 @@ export default class SetNickname extends Command {
             aliases: ['setnickname', 'setnick'],
             category: 'Moderation',
             description: {
-                    content: `Manually set a user's nickname (if blank, resets nickname)`,
+                    content: 'Manually set a user\'s nickname (if blank, resets nickname)',
                     usage: 'setnick [@user] [newnick]',
                     examples: ['setnick @user nickname', 'setnickname @user']
             },
             channel: 'guild',
-            userPermissions: ['MANAGE_NICKNAMES'],
             ratelimit: 3,
             args: [
                 {
@@ -29,10 +27,19 @@ export default class SetNickname extends Command {
         })
     }
 
+    //@ts-ignore
+    userPermissions(message: Message) {
+        const modRole = this.client.settings.get(message.guild, 'modRole', '')
+        const hasStaffRole = message.member.permissions.has('MANAGE_NICKNAMES', true) || message.member.roles.cache.has(modRole)
+
+        if (!hasStaffRole) return 'Moderator'
+        return null
+    }
+
     public async exec(message: Message, {member, nickname}: {member: GuildMember, nickname: string}): Promise<Message> {
-        if (!member.manageable) return message.util!.send('I can not change this user\'s nickname, you should move my role above theirs!')
+        if (!member.manageable) return message.channel.send('I can not change this user\'s nickname, you should move my role above theirs!')
 
         await member.setNickname(nickname)
-        return message.util!.send(`I have set this user\'s nickname successfully.`)
+        return message.channel.send('I have set this user\'s nickname successfully.')
     }
 }
