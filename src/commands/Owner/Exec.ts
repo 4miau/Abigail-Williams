93f02196349839e88ae26309e297add7a5609540
miau <notmiau@pm.me>
2021-11-25 21:14:17 +0000

    FIX: uses spawn instead of exec, less freezing

--- src/commands/Owner/Exec.ts
+++ src/commands/Owner/Exec.ts
@@ -1,6 +1,6 @@
 import { Command } from 'discord-akairo'
 import { Message } from 'discord.js'
-import { execSync } from 'child_process'
+import { spawn } from 'child_process'
 
 export default class Exec extends Command {
     public constructor() {
@@ -25,24 +25,24 @@ export default class Exec extends Command {
     }
 
     private cutString(str: string, max = 30) {
-        if (str.length > max) return `${str.substr(0, max)}...`;
-        return str;
+        if (str.length > max) return `${str.substr(0, max)}...`
+        return str
     }
 
     public exec(message: Message, {commands}: {commands: string}): Promise<any> {
-        if (!commands) return message.util!.send('There is nothing for me to exactly run here...')
+        if (!commands) return message.channel.send('There is nothing for me to exactly run here...')
 
         try {
-            let executed = execSync(commands).toString()
+            let executed = spawn(commands, { shell: true, windowsHide: true, timeout: 4000, serialization: 'advanced' }).toString()
             if (executed) return message.react('✅')
 
             if (executed.length > 2000) {
-                return message.util.send(`\`\`\`prolog\n$ ${message.util.parsed.content}\n\n${this.cutString(executed, 1970 - message.util.parsed.content.length)}\`\`\``);
+                return message.channel.send(`\`\`\`prolog\n$ ${message.util.parsed.content}\n\n${this.cutString(executed, 1970 - message.util.parsed.content.length)}\`\`\``)
             }
-            return message.util.send(`\`\`\`prolog\n$ ${message.util.parsed.content}\n\n${executed}\`\`\``);
+            return message.channel.send(`\`\`\`prolog\n$ ${message.util.parsed.content}\n\n${executed}\`\`\``)
         } catch (e) {
             this.client.logger.log('ERROR', `There was an error running the exec code. Message: ${e.message}`)
-            return message.util!.send(`\`\`\`prolog\n${message.util!.parsed.content}\n\n${e.message}\`\`\``)
+            return message.channel.send(`\`\`\`prolog\n${message.util.parsed.content}\n\n${e.message}\`\`\``)
         }
     }
 }
