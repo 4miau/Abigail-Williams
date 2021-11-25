829665b3422e0a70b77ce049f45c77f1b9dd7446
miau <notmiau@pm.me>
2021-11-25 18:33:32 +0000

    UPD: New logger & modutil

---
+++ src/util/structures/Logger.ts
@@ -0,0 +1,41 @@
+import path from 'path'
+import { createLogger, format, transports} from 'winston'
+
+export const Logger = createLogger({
+    format: format.combine(
+        format.errors({ 'stack': true }),
+        format.label({ 'label': 'BOT'}),
+        format.timestamp({ 'format': 'YYYY/MM/DD HH:mm:ss'}),
+        format.colorize(),
+        format.printf((info: any): string => {
+            const { timestamp, label, level, message, ...rest } = info
+            return `[${timestamp}][${label}][${level}]: ${message}${
+                Object.keys(rest).length ? `\n${JSON.stringify(rest, null, 2)}`: ''
+            }`
+        })
+    ),
+    levels: {
+        'ERROR': 0,
+        'WARN': 1,
+        'CAUTION': 2,
+        'INFO': 3,
+        'DEBUG': 4
+    },
+    transports: [
+        new transports.Console({
+            format: format.colorize({ 'level': true, 'colors': {
+                'ERROR': 'red',
+                'WARN': 'yellow',
+                'CAUTION': 'green',
+                'INFO': 'gray',
+                'DEBUG': 'cyan'
+            }}),
+            level: 'INFO'
+        }),
+
+        new transports.File({ 
+            filename:  path.join(__dirname, '..', '..', 'abby-log'),
+            level: 'DEBUG',
+        })
+    ]
+})
