e646891fd8ae5f921396dcc67bc587f4cd61cf41
miau <notmiau@pm.me>
2021-11-25 21:34:48 +0000

    UPD: greatly improved method

--- src/commands/Owner/Restart.ts
+++ src/commands/Owner/Restart.ts
@@ -1,5 +1,8 @@
 import { Command } from 'discord-akairo'
 import { Message } from 'discord.js'
+import fs from 'fs'
+
+import { findFile, extractEvent } from '../../util/functions/fileaccess'
 
 export default class Restart extends Command {
     public constructor() {
@@ -8,33 +11,48 @@ export default class Restart extends Command {
             category: 'Owner',
             description: {
                     content: 'Restarts a command (or all commands & listeners)',
-                    usage: ['restart [command]'],
-                    examples: ['restart ping', 'reload all']
+                    usage: ['restart [file] <type>'],
+                    examples: ['restart ping', 'reload all', 'restart messagecreate event']
             },
             ownerOnly: true,
             ratelimit: 3,
             args: [
                 {
-                    id: 'command',
-                    type: 'commandAlias',
-                    match: 'rest',
-                    default: (msg: Message) => msg.util.reply('Unable to find that command')
+                    id: 'file',
+                    type: 'string',
+                    match: 'phrase'
+                },
+                {
+                    id: 'type',
+                    type: (_: Message, str: string) => { return (str.caseCompare('command', 'event', 'inhibitor')) ? str.toLocaleLowerCase() : 'command' }, 
+                    match: 'phrase',
                 }
             ]
         })
     }
 
-    public exec(message: Message, {command}: {command: string}): Promise<Message> {
-        if (command === 'all') {
+    public async exec(message: Message, {file, type}: {file: string, type: string}): Promise<Message> {
+        if (!file) return message.channel.send('You must provide a command, event or inhibitor.')
+
+        if (file === 'all') {
             this.client.commandHandler.reloadAll()
+            this.client.inhibitorHandler.reloadAll()
             this.client.listenerHandler.reloadAll()
-            return message.util!.reply('All commands & listeners have been restarted.')
+            return message.reply('All commands, inhibitors & listeners have been restarted.')
         } else {
             try {
-                this.client.commandHandler.reload(command)
-                return message.util!.reply(`${command} has been restarted successfully.`)
+                const loc = await findFile(file, type)
+
+                if (typeof loc === 'string' && loc.includes('/')) {
+                    if (type === 'command') this.client.commandHandler.reload(file)
+                    else if (type === 'event') this.client.listenerHandler.reload(extractEvent(loc))
+                    else if (type === 'inhibitor') this.client.inhibitorHandler.reload(file)
+
+                    return message.channel.send(`Restarted \`${file.toLocaleLowerCase()}\` successfully`)
+                }
+                else throw new Error(loc)
             } catch (err) {
-                return message.util!.reply('Unable to restart this command.')
+                return message.channel.send(`There was an error querying for this command.\nMessage: ${err?.message}`)
             }
         }
     }
