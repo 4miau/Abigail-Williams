6344a2682f7621c9e8f84f3d6469736b80086a9b
miau <notmiau@pm.me>
2021-11-25 18:45:02 +0000

    UPD: Uses mongoDB, category changed

--- src/commands/Configuration/EditGiveaway.ts
+++ src/commands/Configuration/EditGiveaway.ts
@@ -1,16 +1,14 @@
 import { Argument, Command } from 'discord-akairo'
-import { Message, MessageEmbed, TextChannel } from 'discord.js'
-import moment from 'moment'
+import { Message } from 'discord.js'
 
-import { Giveaways } from '../../models/Giveaways'
-import { Colours } from '../../util/Colours'
-import ms from 'ms'
+import Giveaways from '../../models/Giveaways'
+import GiveawayManager from '../../structures/GiveawayManager'
 
 export default class EditGiveaway extends Command {
     public constructor() {
         super('editgiveaway', {
             aliases: ['editgiveaway'],
-            category: 'General',
+            category: 'Configuration',
             description: {
                 content: 'Manage an existing giveaway.',
                 usage: 'editgiveaway [messageID] [prize/time/winners] [value]',
@@ -43,105 +41,28 @@ export default class EditGiveaway extends Command {
 
     //@ts-ignore
     userPermissions(message: Message) {
-        const modRole: string = this.client.settings.get(message.guild, 'modRole', '')
-        const hasStaffRole = message.member.hasPermission('MANAGE_GUILD', { checkAdmin: true, checkOwner: true}) || message.member.roles.cache.has(modRole)
+        const modRole = this.client.settings.get(message.guild, 'modRole', '')
+        const hasStaffRole = message.member.permissions.has('MANAGE_GUILD', true) || message.member.roles.cache.has(modRole)
 
         if (!hasStaffRole) return 'Moderator'
         return null
     }
 
     public async exec(message: Message, {msgID, type, newValue}: {msgID: Message, type: string, newValue: any}): Promise<Message> {
-        if (!msgID) return message.util!.send('Provide a message with a giveaway so I can edit it...')
-        if (!type) return message.util!.send('Well uh, how will I know what to change about the giveaway, huh?')
-        if (!newValue) return message.util!.send('Uhm, no new value for the type? That\'s unhelpful.')
-
-        const giveawayRepo = this.client.db.getRepository(Giveaways)
-        const currGiveaway = await giveawayRepo.findOne({ message: msgID.id })
-
-        if (!currGiveaway) return message.util!.send('This message does not have a giveaway attached to it...')
-
-        const channel = message.guild.channels.resolve(currGiveaway.channel) as TextChannel
-
-        const editGiveaway = {
-            'prize' : async () => {
-                if (typeof newValue !== 'string' || newValue.length < 4) {
-                    message.channel!.send('The new value to replace the message needs to be a string (greater than 4 characters).')
-                    return;
-                }
-
-                await channel.messages.fetch({ limit: 100 })
-                    .then(msgCol => msgCol.find(msg => msg.id === currGiveaway.message))
-                    .then(async msg => {
-                        const e = new MessageEmbed()
-                            .setAuthor(`Giveaway | ${newValue}`)
-                            .setColor(Colours.Spring)
-                            .setDescription(`React with 🎉 to enter!
-                                Giveaway is hosted by ${message.author}!
-                            `)
-                            .setFooter(`Ends at • ${moment(currGiveaway.end).utcOffset(1).format('YYYY/M/DD HH:mm:ss')} ${currGiveaway.winners > 1 ? ' | ' + currGiveaway.winners + ' winners' : ''}`)
-
-                            await msg.edit(e)
-                            await message.channel!.send('Updated the prize of the provided giveaway!')
-
-                            await giveawayRepo.save(currGiveaway)
-                    })
-                    .catch(() => message.util!.send('Failed to update the prize, please try again.'))
-                
-            },
-            'time' : async () => { 
-                if (!Number(ms(newValue))) {
-                    message.channel!.send('The new value to add on to the time needs to be a number (e.g. 3d, 30s)')
-                    return;
-                }
-
-                currGiveaway.end = Date.now() + (Number(ms(newValue)))
-
-                await channel.messages.fetch({ limit: 100 })
-                    .then(msgCol => msgCol.find(msg => msg.id === currGiveaway.message))
-                    .then(async msg => {
-                        const e = new MessageEmbed()
-                            .setAuthor(`Giveaway | ${currGiveaway.reward}`)
-                            .setColor(Colours.Spring)
-                            .setDescription(`React with 🎉 to enter!
-                                Giveaway is hosted by ${message.author}!
-                            `)
-                            .setFooter(`Ends at • ${moment(currGiveaway.end).utcOffset(1).format('YYYY/M/DD HH:mm:ss')} ${currGiveaway.winners > 1 ? ' | ' + currGiveaway.winners + ' winners' : ''}`)
-
-                        await msg.edit(e)
-                        await message.channel!.send('Updated the end time of the provided giveaway!')
-
-                        await giveawayRepo.save(currGiveaway)
-                    })
-                    .catch(() => message.util!.send('Failed to update the prize, please try again.'))
-            },
-            'winners' : async () => { 
-                if (typeof newValue !== 'number') {
-                    message.channel!.send('The new value to replace the number of winners needs to be a number.')
-                    return;
-                }
-
-                currGiveaway.winners = (newValue as number)
-
-                await channel.messages.fetch({ limit: 100 })
-                    .then(msgCol => msgCol.find(msg => msg.id === currGiveaway.message))
-                    .then(async msg => {
-                        const e = new MessageEmbed()
-                            .setAuthor(`Giveaway | ${currGiveaway.reward}`)
-                            .setColor(Colours.Spring)
-                            .setDescription(`React with 🎉 to enter!
-                                Giveaway is hosted by ${message.author}!
-                            `)
-                            .setFooter(`Ends at • ${moment(currGiveaway.end).utcOffset(1).format('YYYY/M/DD HH:mm:ss')} ${currGiveaway.winners > 1 ? ' | ' + currGiveaway.winners + ' winners' : ''}`)
-
-                        await msg.edit(e)
-                        await message.channel!.send('Updated the number of winners for the provided giveaway!')
-
-                        await giveawayRepo.save(currGiveaway)
-                    })
-                    .catch(() => message.util!.send('Failed to update the prize, please try again.'))
+        if (!msgID) return message.channel.send('Provide a message with a giveaway so I can edit it...')
+        if (!type) return message.channel.send('Well uh, how will I know what to change about the giveaway, huh?')
+        if (!newValue) return message.channel.send('Uhm, no new value for the type? That\'s unhelpful.')
+        
+        try {
+            const giveaway = await Giveaways.findOne({ message: msgID.id })
+            if (giveaway) {
+                this.client.queue.add(GiveawayManager.edit(msgID, { type, newValue }))
+                message.delete()
+                return message.channel.send('Giveaway edited successfully.')
             }
+            else throw new Error('Unable to find the giveaway.')
+        } catch {
+            return message.channel.send('This message does not have a giveaway attached to it...')
         }
-
-        this.client.queue.add(editGiveaway[type].call())
     }
 }
