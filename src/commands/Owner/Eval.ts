5aa1f5193cac336aa663f0c658271e8ca81783c4
miau <notmiau@pm.me>
2021-11-25 21:13:43 +0000

    UPD: Supports TS code

--- src/commands/Owner/Eval.ts
+++ src/commands/Owner/Eval.ts
@@ -1,6 +1,7 @@
 import { Command } from 'discord-akairo'
 import { Message, MessageEmbed } from 'discord.js'
 import { performance } from 'perf_hooks'
+import * as ts from 'typescript'
 
 export default class Eval extends Command {
     public constructor() {
@@ -9,7 +10,7 @@ export default class Eval extends Command {
             category: 'Owner',
             description: {
                     content: 'Evaluates and executes Javascript code.',
-                    usage: 'eval <JS code>',
+                    usage: 'eval <JS/TS code>',
                     examples: ['eval console.log(\'hi\')']
             },
             ownerOnly: true,
@@ -28,35 +29,44 @@ export default class Eval extends Command {
         const bannedEvalWords = [
             'this.client.token',
             'token',
-            'api',
             'spotifyclient',
             'host',
-            'port',
-            'password',
+            'password'
         ]
 
-        if (bannedEvalWords.some(s => message.content.toLowerCase().includes(s))) return message.util!.send('You\'re a little cheeky one, aren\'t ya, master..')
+        let evalCode = message.util.parsed.content
 
-        const reply = await message.util.send(new MessageEmbed()
+        if (bannedEvalWords.some(s => evalCode.toLowerCase().includes(s))) return message.channel.send('You\'re a little cheeky one, aren\'t ya, master..')
+
+        if (evalCode.startsWith('```js')) evalCode = evalCode.substring(5, evalCode.length - 3)
+        else if (evalCode.startsWith('```ts')) {
+            evalCode = evalCode.substring(5, evalCode.length - 3)
+            evalCode = ts.transpile(evalCode)
+        }
+
+        const e = new MessageEmbed()
             .setTitle('Processing JavaScript code...')
             .setColor('BLUE')
-        )
+
+        const reply = await message.channel.send({ embeds: [e] })
 
         const start = performance.now()
 
         try {
-            const output = eval(message.util.parsed.content)
-            reply.edit(new MessageEmbed()
+            const output = eval(evalCode)
+            const e = new MessageEmbed()
                 .setTitle(`Completed in ${(performance.now() - start).toFixed(3)}ms`)
                 .setColor('GREEN')
                 .addField('Returns', `\`\`\`javascript\n${output}\`\`\``)
-            )
+
+            reply.edit({ embeds: [e] })
         } catch (err) {
-            reply.edit(new MessageEmbed()
+            const e = new MessageEmbed()
                 .setTitle(`Error in ${(performance.now() - start).toFixed(3)}ms`)
                 .setColor('RED')
                 .addField('Error', `\`\`\`javascript\n${err}\`\`\``)
-            )
+
+            reply.edit({ embeds: [e] })
         }
     }
 }
