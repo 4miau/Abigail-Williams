5b039f6edc2efd1319f8871aa7a16a9b48387536
miau <notmiau@pm.me>
2021-11-25 19:20:11 +0000

    ADD: Moved from commands/owner

---
+++ src/commands/Developer/ChangeLogAdd.ts
@@ -0,0 +1,49 @@
+import { Argument, Command } from 'discord-akairo'
+import { Message } from 'discord.js'
+
+export default class ChangeLogAdd extends Command {
+    public constructor() {
+        super('changelogadd', {
+            aliases: ['changelogadd', 'addtochangelog', 'addchangelog'],
+            category: 'Developer',
+            description: {
+                content: 'Adds an entry to the changelog.',
+                usage: 'changelogadd [type] [content]',
+                examples: ['changelogadd add test'],
+                tags: [
+                    'ADD - Add, new content/a feature was added.', 
+                    'DEL - Delete, some content/a feature has been removed.', 
+                    'UPD - Update, a feature has been updated.', 
+                    'FIX - Fix, some content/feature has been fixed.', 
+                    'CHO - Chore, code for a feature has been cleaned/optimized.'
+                ]
+            },
+            ownerOnly: true,
+            ratelimit: 3,
+            args: [
+                {
+                    id: 'type',
+                    type: Argument.union('changelog'),
+                    match: 'phrase'
+                },
+                {
+                    id: 'content',
+                    type: 'string',
+                    match: 'rest'
+                }
+            ]
+        })
+    }
+
+    public exec(message: Message, {type, content}: {type: string, content: string}): Promise<Message> {
+        if (!type) return message.util.send('You need to provide a valid type to add something to the changelog.')
+
+        const changeLog: { key: number, type: string, content: string }[] = this.client.settings.get('global', 'changeLog', [])
+        const key = changeLog.length === 0 ? 1 : (changeLog.length + 1)
+
+        changeLog.push({ key: key, type: type, content: content })
+        this.client.settings.set('global', 'changeLog', changeLog)
+
+        return message.util.send('Changelog successfully added.')
+    }
+}
---
+++ src/commands/Developer/Notice.ts
@@ -0,0 +1,50 @@
+import { Command } from 'discord-akairo'
+import { MessageEmbed, TextChannel, Message } from 'discord.js'
+import moment from 'moment'
+
+import { Colours } from '../../util/Colours'
+
+export default class Notice extends Command {
+    public constructor() {
+        super('notice', {
+            aliases: ['notice', 'newnotice'],
+            category: 'Developer',
+            description: {
+                content: 'Creates a new notice message for servers.',
+                usage: 'notice [content]',
+                examples: ['notice New bot feature!'],
+            },
+            ownerOnly: true,
+            channel: 'guild',
+            ratelimit: 3,
+            args: [
+                {
+                    id: 'content',
+                    type: 'string',
+                    match: 'rest'
+                }
+            ]
+        })
+    }
+
+    public async exec(message: Message, {content}: {content: string}): Promise<Message> {
+        if (!content) return message.util.send('You might want to add a message to send as a notice.')
+
+        this.client.guilds.cache.forEach(async g => {
+            const noticeChannel = g.channels.resolve(this.client.settings.get(g, 'server-notices', '')) as TextChannel
+
+            if (!noticeChannel) return
+
+            const e = new MessageEmbed()
+                .setAuthor('New Bot Notice | Abigail Williams', this.client.user.displayAvatarURL({ dynamic: true, format: 'png' }))
+                .setThumbnail(this.client.user.displayAvatarURL({ dynamic: true, format: 'png' }))
+                .setColor(Colours.Purple)
+                .setDescription(content)
+                .setFooter(`Powered by the cutie Miau - ${moment(message.createdAt).utcOffset(1).format('YYYY/MM/DD HH:mm:ss')}`)
+
+            await noticeChannel.send({ embeds: [e] })
+        })
+        
+        return message.util.send('New announcement has been posted to all channels that have a notice channel set.')
+    }
+}
