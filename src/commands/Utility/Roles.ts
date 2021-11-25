bc277023b696430e7ab7ffd099a8f949696741ab
miau <notmiau@pm.me>
2021-11-25 21:20:06 +0000

    UPD: use of custom prototype methods

--- src/commands/Utility/Roles.ts
+++ src/commands/Utility/Roles.ts
@@ -2,7 +2,6 @@ import { Command } from 'discord-akairo'
 import { GuildMember, Message, MessageEmbed } from 'discord.js'
 
 import { Colours } from '../../util/Colours'
-import { chunkNewLine } from '../../util/Functions'
 
 export default class Roles extends Command {
     public constructor() {
@@ -32,26 +31,35 @@ export default class Roles extends Command {
 
     public async exec(message: Message, {member, server}: {member: GuildMember, server: string}): Promise<Message> {
         if (server === 'server') {
-            return await message.util!.send(new MessageEmbed()
+            const e = new MessageEmbed()
                 .setAuthor(`${message.guild.name}`, message.guild.iconURL())
                 .setThumbnail(message.guild.iconURL())
                 .setColor(Colours.SkyBlue)
                 .addField(`**${message.guild.name}'s total roles:** [${message.guild.roles.cache.size - 1}]`, `${member.guild.roles.cache.size - 1 > 0 ?
-                    chunkNewLine(message.guild.roles.cache.map(r => r)
-                    .sort((a, b) => b.position - a.position)
-                    .map(role => role.name )
-                    .filter(name => name !== '@everyone'), 3).join(', ') : 'None'}`)
-            )
-        }
+                    message.guild.roles.cache
+                        .map(r => r)
+                        .sort((a, b) => b.position - a.position)
+                        .map(role => role.name )
+                        .filter(name => name !== '@everyone')
+                        .chunkNewLine(3)
+                        .join(', ') : 'None'}`)
 
-        return await message.util.send(new MessageEmbed()
-            .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL())
-            .setThumbnail(member.user.displayAvatarURL())
-            .setColor(Colours.SkyBlue)
-            .addField(`**Roles:** [${member.roles.cache.size - 1}]`, `${member.roles.cache.size - 1 > 0 ? chunkNewLine(member.roles.cache.map(r => r)
-                .sort((a, b) => b.position - a.position)
-                .map(role => role.name)
-                .filter(name => name !== '@everyone'), 3).join(', ') : 'None'}`)
-        )
+            return message.channel.send({ embeds: [e] })
+        }
+        else {
+            const e = new MessageEmbed()
+                .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL())
+                .setThumbnail(member.user.displayAvatarURL())
+                .setColor(Colours.SkyBlue)
+                .addField(`**Roles:** [${member.roles.cache.size - 1}]`, `${member.roles.cache.size - 1 > 0 ? member.roles.cache
+                    .map(r => r)
+                    .sort((a, b) => b.position - a.position)
+                    .map(role => role.name)
+                    .filter(name => name !== '@everyone')
+                    .chunkNewLine(3)
+                    .join(', ') : 'None'}`)
+
+            return message.channel.send({ embeds: [e] })
+        }
     }
 }
