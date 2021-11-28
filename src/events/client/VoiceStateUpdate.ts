69d8d52e5eac5f4c59f516fbcb6655da1216c07b
miau <notmiau@pm.me>
2021-11-28 00:14:05 +0000

    UPD: Empty event, saves memory usage

--- src/events/client/VoiceStateUpdate.ts
+++ src/events/client/VoiceStateUpdate.ts
@@ -11,12 +11,5 @@ export default class VoiceStateUpdate extends Listener {
     }
 
     public async exec(oldState: VoiceState, newState: VoiceState): Promise<void> {
-        this.client.music.player._voiceUpdate(oldState, newState)
     }
 }
-
-/*
-        if (oldState.channelId !== oldState.guild.me.voice.channelId || newState.channelId) return
-        
-        if (!(oldState.channel.members.size - 1)) setTimeout(() => { if (!(oldState.channel.members.size - 1)) oldState.guild.me.voice.disconnect() }, 3e5)
-*/
