0d6a813c26f2c3bcbcc373b77597a889fa478a87
miau <notmiau@pm.me>
2021-11-28 00:12:57 +0000

    ADD: danbooru command

--- src/commands/Images/Danbooru.ts
+++ src/commands/Images/Danbooru.ts
@@ -1,7 +1,8 @@
 import { Command } from 'discord-akairo'
-import { Message } from 'discord.js'
+import { Message, MessageEmbed } from 'discord.js'
+import moment, { now } from 'moment'
 
-import { _DanbooruQuery } from '../../util/Functions'
+import { _GetBooruImages } from '../../util/functions/danbooru'
 
 export default class Danbooru extends Command {
     public constructor() {
@@ -10,21 +11,39 @@ export default class Danbooru extends Command {
             category: 'Images',
             description: {
                 content: 'Get images from danbooru',
-                usage: 'danbooru (tag)',
-                examples: ['danbooru dog'],
+                usage: 'danbooru [tag] <-amount=amount>',
+                examples: ['danbooru shimakaze', 'danbooru shimakaze -amount=5'],
             },
+            ownerOnly: true,
             ratelimit: 3,
             args: [
                 {
                     id: 'tag',
-                    type: 'string'
+                    type: 'string',
+                    match: 'rest'
+                },
+                {
+                    id: 'amount',
+                    type: 'number',
+                    match: 'option',
+                    flag: '-amount='
                 }
             ]
         })
     }
 
-    public async exec(message: Message): Promise<Message> {
-        console.log(await _DanbooruQuery('ganyu_(genshin_impact)'))
-        return message.util!.send('hi')
+    public async exec(message: Message, {tag, amount}: {tag: string, amount: number}): Promise<Message> {
+        if (!tag) return message.channel.send('Must provide a tag.')
+
+        const genImage = await _GetBooruImages(tag, amount ? amount : void 0)
+        if (!genImage) return message.channel.send('Unable to find any images using this tag, please try again.')
+
+        const e = new MessageEmbed()
+            .setDescription('A generated image from Danbooru.')
+            .setColor('RANDOM')
+            .setImage(genImage.file_url.endsWith('.zip') ? genImage.large_file_url : genImage.file_url)
+            .setFooter(`Command ran by ${message.author.tag} • ${moment(now()).utcOffset(1).format('YYYY/MM/DD HH:mm:ss')}`)
+        
+        return message.channel.send({ embeds: [e] })
     }
 }
