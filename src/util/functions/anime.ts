3ab6ab21eda508f3e2056de6d4675a5cdc5e9b40
miau <notmiau@pm.me>
2022-04-25 01:27:03 +0000

    remove: services are now used, outdated

--- src/util/functions/anime.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export async function _GetAnimeSFW(category: string): Promise<any> {
-    try { return await axios.get(`https://api.waifu.pics/sfw/${category}`).then(res => res.data).catch(() => void 0) }
-    catch { return null }
-}
