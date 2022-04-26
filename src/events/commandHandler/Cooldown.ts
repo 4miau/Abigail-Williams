7175229bc61f0b183609eb3f175608092c34af07
miau <notmiau@pm.me>
2022-04-26 00:11:42 +0000

    edit: deletes original message

--- src/events/commandHandler/Cooldown.ts
+++ src/events/commandHandler/Cooldown.ts
@@ -10,7 +10,8 @@ export default class Cooldown extends Listener {
         })
     }
 
-    public exec(message: Message, command: Command, remaining: number) {
-        return message.channel.send(`You're on a cooldown! You will have to wait for \`${(remaining / 1000).toFixed(0)}\`seconds to run this command again.`)
+    public async exec(message: Message, command: Command, remaining: number) {
+        const msg = await message.channel.send(`You're on a cooldown! You will have to wait for \`${(remaining / 1000).toFixed(0)}\`seconds to run this command again.`)
+        setTimeout(() => { msg.delete() }, 1e4)
     }
 }
